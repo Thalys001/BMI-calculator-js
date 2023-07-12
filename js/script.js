68df588d1fbfa3645e3a193aed7791f58adbd602
Thalys001 <thalyssouza708@gmail.com>
2023-07-11 23:57:49 -0300

    refactor: added clean code rules

--- js/script.js
+++ js/script.js
@@ -1,32 +1,38 @@
 import { Modal } from "./modal.js";
 import { AlertError } from "./alert-error.js";
-import { notNumber, IMC } from "./utils.js";
+import { notNumber, calculateIMC } from "./utils.js";
 
 const form = document.querySelector("form");
 const inputWeight = document.querySelector("#weight");
 const inputHeight = document.querySelector("#height");
 
+inputWeight.oninput = () => AlertError.close();
+inputHeight.oninput = () => AlertError.close();
 form.onsubmit = (event) => {
   event.preventDefault();
 
   const weight = inputWeight.value;
   const height = inputHeight.value;
 
-  const showAlertError = notNumber(weight) || notNumber(height);
+  const weightOrHeightIsNotNumber = notNumber(weight) || notNumber(height);
 
-  if (showAlertError) {
+  if (weightOrHeightIsNotNumber) {
     AlertError.open();
     return;
   }
 
   AlertError.close();
 
-  const result = IMC(weight, height);
+  const result = calculateIMC(weight, height);
+  displayResultMessage(result);
+};
+
+function displayResultMessage(result) {
   const message = `Seu IMC é de ${result}`;
 
   Modal.message.innerText = message;
   Modal.open();
-};
+}
 
 // 3 maneiras de criar e atribuir função a um evento
 // - 1
